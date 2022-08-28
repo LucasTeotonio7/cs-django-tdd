3e106383bd67d34dac72c1b0e1ee2ffc5e5952e9
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-28 09:52:28 -0300

    feat: created test to check browser

--- setup/tests.py
+++ setup/tests.py
@@ -4,8 +4,13 @@ from selenium import webdriver
 class AnimalsTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome('/home/lucas/workspace/cs-django-tdd/chromedriver.exe')
+        PATH = "/home/lucas/workspace/cs-django-tdd/chromedriver"
+        self.browser = webdriver.Chrome(PATH)
 
 
     def tearDown(self):
         self.browser.quit()
+
+
+    def test_check_browser_window(self):
+        self.browser.get("https://www.google.com/")
