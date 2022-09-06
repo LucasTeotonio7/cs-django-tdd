c749c88e861619c6b2c3d1d84ce9657604edff43
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-09-05 22:27:03 -0300

    chore: commented setup tests

--- setup/tests.py
+++ setup/tests.py
@@ -1,16 +1,16 @@
-from django.test import LiveServerTestCase
-from selenium import webdriver
+# from django.test import LiveServerTestCase
+# from selenium import webdriver
 
-class AnimalsTestCase(LiveServerTestCase):
+# class AnimalsTestCase(LiveServerTestCase):
 
-    def setUp(self):
-        PATH = "/home/lucas/workspace/cs-django-tdd/chromedriver"
-        self.browser = webdriver.Chrome(PATH)
-
-
-    def tearDown(self):
-        self.browser.quit()
+#     def setUp(self):
+#         PATH = "/home/lucas/workspace/cs-django-tdd/chromedriver"
+#         self.browser = webdriver.Chrome(PATH)
 
 
-    def test_check_browser_window(self):
-        self.browser.get("https://www.google.com/")
+#     def tearDown(self):
+#         self.browser.quit()
+
+
+#     def test_check_browser_window(self):
+#         self.browser.get("https://www.google.com/")
