4a65f72b65ce5a22bc633ce0f82675ac2bb8f971
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-09-05 22:28:59 -0300

    feat: created tests for the urls of animals

---
+++ animals/tests/tests_urls.py
@@ -0,0 +1,17 @@
+from urllib import response
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+
+from animals.views import index
+
+class AnimalsURLSTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        return super().setUp()
+
+    def test_route_use_view_index(self):
+        "tests if the application uses the index function of the view"
+        request = self.factory.get('/')
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
