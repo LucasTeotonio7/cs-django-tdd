57b552c7dc25c9b8e845312d6620fe56b7613ea4
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-09-10 20:47:56 -0300

    feat: created test for the characteristics

--- animals/tests/tests_views.py
+++ animals/tests/tests_views.py
@@ -1,15 +1,24 @@
 from django.db.models.query import QuerySet
 from django.test import TestCase, RequestFactory
+from animals.models import Animal
 
 
 class IndexViewTestCase(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
+        self.animal = Animal.objects.create(
+            name ='Dog',
+            predator=False,
+            venomous=False,
+            domestic=True,
+        )
 
     def test_view_index_return_animal_characteristics(self):
         response = self.client.get('/',
-            {'characteristics' : 'result'}
+            {'search' : 'Dog'}
         )
+        researched_animal = response.context['characteristics']
         self.assertIs(type(response.context['characteristics']), QuerySet)
+        self.assertEqual(researched_animal[0].name, 'Dog')
 
