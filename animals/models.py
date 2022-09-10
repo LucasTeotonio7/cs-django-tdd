1cf89d18dc03c078e1659fcca074fe2be8f3aefc
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-09-10 20:21:53 -0300

    feat: create model Animal

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField(max_length=20)
+    predator = models.BooleanField()
+    venomous = models.BooleanField()
+    domestic = models.BooleanField()
+
+
+    def __str__(self):
+        return self.name
